50e120c6a6cd8d5a2385eadafed1fa50c39066e4
tapos-chandro <taposrj85@gmail.com>
2025-07-17 22:08:45 +0600

    show password button set

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router';
+import { IoEye } from "react-icons/io5";
+import { IoMdEyeOff } from "react-icons/io";
+
 
 const Login = () => {
+    const [showPass ,setShowPass]=useState(false)
+
+    const handelUserLogin =(e)=>{
+        e.preventDefault()
+        const email= e.target.email.value ;
+        const password = e.target.password.value;       
+        
+    }
     return (
         <div className="hero bg-sky-200 ">
-            <form >
+            <form onSubmit={handelUserLogin} >
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <div className="text-center lg:text-left">
                         <h1 className="text-5xl font-bold text-sky-700" >Login now!</h1>
@@ -17,13 +28,23 @@ const Login = () => {
                         <div className="card-body">
                             <fieldset className="fieldset">
                                 <label className="label">Email</label>
-                                <input type="email" className="input" placeholder="Email" />
-                                <label className="label">Password</label>
-                                <input type="password" className="input" placeholder="Password" />
+                                <input name='email' type="email" className="input" placeholder="Email" />
+                                
+                                <label className="label relative">Password</label>
+                                <input name='password' type={showPass ? "text" : "password"} className="input" placeholder="Password" />
+                                <div onClick={()=>setShowPass(!showPass)} className='absolute mt-[105px] mx-72 text-xl text-blue-700'>
+                                        {
+                                            showPass ? <IoEye   /> : <IoMdEyeOff />
+
+                                        }
+                                </div>
+                                
+
+
                                 <div><a className="link link-hover">Forgot password?</a></div>
                                 <button className="btn btn-info mt-4">Login</button>
                                 <div>
-                                    <h1>You Hove No Account Please <span className='text-sky-800 font-bold'><Link to={"/register"}> Register</Link></span> </h1>
+                                    <h1>You Hove No Account Please <span className='text-sky-800 font-bold underline'><Link to={"/register"}> Register</Link></span> </h1>
                                 </div>
                             </fieldset>
                         </div>
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Register = () => {
     return (
-        <div>
-            
+        <div className="hero bg-sky-200 ">
+            <form >
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <div className="text-center lg:text-left">
+                        <h1 className="text-5xl font-bold text-sky-700" >Register now!</h1>                      
+                    </div>
+                    <div className="card bg-sky-300 w-full max-w-sm shrink-0 shadow-2xl">
+                        <div className="card-body">
+                            <fieldset className="fieldset">
+                                <label className="label">Name</label>
+                                <input type="text" name='name' className="input" placeholder="Name" />
+
+                                <label className="label">Email</label>
+                                <input type="email" name='email' className="input" placeholder="Email" />
+
+                                <label className="label">Photo</label>
+                                <input type="text" name='photo' className="input" placeholder="Photo URL" />
+
+                                <label className="label">Password</label>
+                                <input type="password" name='password' className="input" placeholder="Password" />
+
+                                <div><a className="link link-hover">Forgot password?</a></div>
+                                <button className="btn btn-info mt-4 font-bold">Register</button>
+                                <div>
+                                    <h1>You Hove No Account Please <span className='text-sky-800 font-bold underline'><Link to={"/login"}> Login</Link></span> </h1>
+                                </div>
+                            </fieldset>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     );
 };
